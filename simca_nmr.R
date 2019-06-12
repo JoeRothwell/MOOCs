fdad4f02cd926af76bb6786e7ac8a43ed6f38f06
JoeRothwell <joerothwell1@gmail.com>
2019-06-12 17:43:24 +0200

    Improved variable names

--- simca_nmr.R
+++ simca_nmr.R
@@ -12,17 +12,17 @@ pca.nmr <- pca(X, ncomp = 10, center = F, scale = F)
 plotIndiv(pca.nmr, group = Y, ind.names = F, legend = T, title = 'NMR data')
 
 # PLS-DA with 5 components (arbitrary choice)
-plsda.res <- plsda(X, Y, ncomp = 5) # where ncomp is the number of components wanted
+plsda.initial <- plsda(X, Y, ncomp = 5) # where ncomp is the number of components wanted
 
 # Compute evaluation criteria
 set.seed(2543) # for reproducibility here, only when the `cpus' argument is not used
-perf.plsda <- perf(plsda.res, validation = "Mfold", folds = 5, 
-                   progressBar = FALSE, auc = TRUE, nrepeat = 10) 
+modeval <- perf(plsda.initial, validation = "Mfold", folds = 5, progressBar = F, auc = T, nrepeat = 10) 
+# 3 components seem to be best
 
 # Plot results
-plot(perf.plsda, col = color.mixo(1:3), sd = TRUE, legend.position = "horizontal")
+plot(modeval, col = color.mixo(1:3), sd = T, legend.position = "horizontal")
 
-plsda.res <- plsda(X, Y, ncomp = 3) # where ncomp is the number of components wanted
+plsda.comp <- plsda(X, Y, ncomp = 3) # where ncomp is the number of components wanted
 auroc(plsda.res, roc.comp = 3)
 
 plotLoadings(plsda.res, comp = 3, title = 'Loadings on comp 3', 
